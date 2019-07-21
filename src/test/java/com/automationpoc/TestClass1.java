bdf889ce4939db6c88164839c29d5a67176481e3
Josh Adams <jadams@hagerty.com>
2019-07-20 21:26:56 -0400

    init

---
+++ src/test/java/com/automationpoc/TestClass1.java
@@ -0,0 +1,46 @@
+package com.automationpoc;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestClass1 {
+
+    public static WebDriver driver;
+
+    @BeforeMethod
+    public void launchDriver(){
+        System.setProperty("webdriver.chrome.driver","/Users/jadams/GitRepos/baseexample-testautomation/resources/binaries/unix/chromedriver");
+        driver  = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().fullscreen();
+
+    }
+
+    @Test
+    public void Test1(){
+        driver.navigate().to("https://www.google.com");
+        System.out.println("test1 title is: " + driver.getTitle());
+    }
+
+    @Test
+    public void Test2(){
+        driver.navigate().to("https://www.google.com");
+        System.out.println("test2 title is: " + driver.getTitle());
+    }
+
+    @Test
+    public void Test3(){
+        driver.navigate().to("https://www.google.com");
+        System.out.println("test3 title is: " + driver.getTitle());
+    }
+
+    @AfterMethod
+    public void quit(){
+        driver.quit();
+    }
+}
